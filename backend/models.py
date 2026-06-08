245306de6253385df66702b5c8c550f2522c201e
eternalisfine <deepyamanm@gmail.com>
2026-06-08 15:12:23 +0530

    User link -> Quest

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,17 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, func
 from database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer,primary_key=True, index=True)
+    email = Column(String,  unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    quests = relationship("Quest", back_populates="owner", cascade="all, delete-orphan")
+
+
 class Quest(Base):
     __tablename__ = "quests"
 
@@ -9,3 +20,6 @@ class Quest(Base):
     duration_minutes = Column(Integer, nullable=False)
     completed = Column(Boolean, default=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    owner = relationship("User", back_populates="quests")
