be10ac097cc1e078f034ff8817d07c6d08e39da8
eternalisfine <deepyamanm@gmail.com>
2026-06-08 15:16:07 +0530

    auth schemas

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,6 +1,19 @@
-from pydantic import BaseModel
-from pydantic import ConfigDict
+from pydantic import BaseModel, EmailStr, ConfigDict
 from datetime import datetime
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    email: str
+    created_at: datetime
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
 
 class QuestCreate(BaseModel):
     title: str
