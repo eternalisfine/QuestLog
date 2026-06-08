047f1b7940fa3f645adef796c8410d2fd3d3b4e0
eternalisfine <deepyamanm@gmail.com>
2026-06-09 03:27:21 +0530

    redis client

---
+++ backend/redis_client.py
@@ -0,0 +1,11 @@
+import redis, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+r = redis.Redis(
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
+    decode_responses=True
+)
