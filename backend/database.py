a04da2d7258ecdaeeb7fa336a53a8d660d056234
eternalisfine <deepyamanm@gmail.com>
2026-06-10 02:43:22 +0530

    new read engine

--- backend/database.py
+++ backend/database.py
@@ -1,20 +1,34 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
-import os
-
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+import os
 
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+READ_DATABASE_URL =os.getenv("READ_DATABASE_URL", DATABASE_URL)
+
+# Primary
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Replica
+read_engine = create_engine(READ_DATABASE_URL)
+ReadSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=read_engine)
 
 class Base(DeclarativeBase):
     pass
 
 def get_db():
     db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_read_db():
+    db = ReadSessionLocal()
     try:
         yield db
     finally:
